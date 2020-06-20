74ec17a2180fbf140ec13fff3a4198851ee9a90c
Amedeo D'Amelio <amedeodam@gmail.com>
2020-06-20 22:31:29 +0200

    Added print

--- Progetto/user_list.py
+++ Progetto/user_list.py
@@ -6,7 +6,9 @@ def get_user_list():
     db_path = "./Dataset/embeddings.pickle"
     if os.path.exists(db_path):
         database = pickle.load(open(db_path, "rb"))
+        print("Gli utenti al momento registrati sono:\n")
         for names in database:
             print(names)
+        print("\n")
     else:
         print("Nessun nome presente nel database")
